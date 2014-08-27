a71ff086fddad8f14486b22df595cf4f29387a22
Krzysztof Spytkowski <kspytkowski@wp.pl>
2014-08-27 23:07:53 +0200

    second part of ex8 done

--- mlclass-ex8/cofiCostFunc.m
+++ mlclass-ex8/cofiCostFunc.m
@@ -40,19 +40,19 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-J = 1 / 2 * sum(sum(((X * Theta' - Y) .* R) .^ 2));
+J = 1 / 2 * sum(sum(((X * Theta' - Y) .* R) .^ 2)) + lambda / 2 * sum(sum(Theta .^ 2)) + lambda / 2 * sum(sum(X .^ 2));
 for i = 1 : size(X)
     idx = find(R(i, :) == 1);
     Theta_temp = Theta(idx, :);
     Y_temp = Y(i, idx);
-    X_grad(i, :) = (X(i, :) * Theta_temp' - Y_temp) * Theta_temp;
+    X_grad(i, :) = (X(i, :) * Theta_temp' - Y_temp) * Theta_temp + lambda * X(i, :);
 end;
-%for i = 1 : size(Theta)
-%    idx = find(R(:, i) == 1);
-%    Theta_temp = Theta(:, i);
-%    Y_temp = Y(idx, i);
-%    Theta_grad(i, :) = (Theta_temp * X(i, :) - Y_temp) * X(i, :);
-%end;
+for j = 1 : size(Theta)
+    idx = find(R(:, j) == 1);
+    Y_temp = Y(idx, j);
+    X_temp = X(idx, :);
+    Theta_grad(j, :) = X_temp' * (X_temp * Theta(j, :)' - Y_temp) + lambda * Theta(j, :)';
+end;
 
 
 % =============================================================
